13e29f1d38e8c397b6c0459a5298f7863a2dd81a
Sahil <sahillal25@outlook.de>
2023-06-18 16:24:35 +0200

    first commit

---
+++ client/script.js
@@ -0,0 +1,177 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+let questions=[];
+let awnsers =[];
+let pCount = 0;
+let loadInterval;
+var name = "";
+var task = "";
+const taskNames =["TaskMoralDilemma","TaskHealthChat","TaskSQL","TaskTPT"];
+
+function isTask(){
+  for(let i = 0; i< taskNames.length;i++){
+    if(task.trim() === taskNames[i]){
+      return true
+    }
+  }
+  return false;
+}
+  name = window.prompt("Geben Sie Ihren Namen ein:");
+  //task = window.prompt("Task: " + "(" + taskNames[0]+","+taskNames[1] + " oder " + taskNames[2]+ ")");
+  task = taskNames[3];
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+      <div class ="wrapper ${isAi && 'ai'}">
+        <div class = "chat">
+          <div class = "profile">
+            <img 
+              src ="${isAi ? bot : user}"
+              alt="${isAi ? 'bot' : 'user'}"
+            />
+          </div>
+          <div class = "message" id = ${uniqueId}>${value}</div>
+        </div>
+      </div>
+    `
+  )
+}
+
+const handleSubmit = async(e) =>{
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  chatContainer.innerHTML += chatStripe(false,data.get('promt'));
+  questions[pCount] = data.get('promt');
+  
+  form.reset();
+
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true," ",uniqueId);
+  
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+  
+  loader(messageDiv);
+  const input = name + " " + data.get('promt');
+  //https://chatbot-pbxf.onrender.com
+  const response = await fetch('https://chatbot-healthchat.onrender.com',{
+    method: 'POST',
+    headers :{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt:data.get('promt'),
+      na:name,
+      ta:taskNames[3],
+    })
+    
+  })
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+  if(response.ok){
+    const data = await response.json();
+    console.log(data.bot)
+    const parsedData = data.bot.content.trim();
+    awnsers[pCount] = parsedData;
+    typeText(messageDiv,parsedData);
+  }else{
+    const err = await response.text();
+    
+    messageDiv.innerHTML = "Something went wrong";
+
+    alert(err);
+  }
+  
+  pCount++;
+}
+function abcdjb(){
+  const date = new Date();
+  return  " " + date.getHours() + date.getFullYear() + date.getMonth() + date.getDay();
+
+}
+function sub(e){
+  console.log(name);
+  if(name.trim() != ""){
+    handleSubmit(e);
+  }
+  else{
+    name =window.prompt("Geben Sie Ihren Namen ein:");
+  }
+}
+form.addEventListener('submit',(e)=>{
+  if(name.trim() != "" && taskNames.includes(task)){
+      handleSubmit(e);
+  }
+  else{
+    if(name.trim() == ""){
+    name =window.prompt("Geben Sie Ihren Namen ein:");
+    }
+    if(!taskNames.includes(task)){
+      task = window.prompt("Task: " + taskNames);
+    }
+  }
+});
+form.addEventListener('keyup', (e)=>{
+  if(e.keyCode === 13){
+  if(name.trim() != ""&& taskNames.includes(task)){
+   
+      handleSubmit(e);
+    }
+    else{
+      if(name.trim() == ""){
+        name =window.prompt("Geben Sie Ihren Namen ein:");
+        }
+        if(taskNames.includes(task)){
+          task = window.prompt("Task: " + taskNames);
+        }
+    }
+  }
+  
+});
+
+const click = function(){
+  if(pCount>= 6){
+    alert("Fb637^");
+  }
+  else{
+    alert("Mindestens 6 prompts!");
+  }
+}
+document.getElementById("code").addEventListener('click',click);
---
+++ server/server.js
@@ -0,0 +1,125 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from 'openai';
+/*import * as sqlite from 'sqlite3';
+const sqlite3 = sqlite;*/
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+//const sqlite3 = require('sqlite3').verbose();
+var pg = require('pg');
+//var conString ="postgres://jjcvxmos:5D9o-SZWVWJRb5a8VPaWojzwHJygUjQz@tyke.db.elephantsql.com/jjcvxmos";
+var conString = process.env.URL;
+
+var name = "";
+var task = "";
+dotenv.config();
+
+function getPrompt(string) {
+    let i = 0;
+    let n = "";
+    for (let k = 0; k < string.length; k++) {
+        if (string.charAt(k) != " ") {
+            n += string.charAt(k);
+            i++;
+        }
+
+    }
+    name = n;
+    i++;
+    let p = "";
+    for (let h = i; h <= string.length; h++) {
+        p += string.charAt(h);
+    }
+    return p;
+}
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: 'Hello from CodeX',
+    })
+});
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        name = req.body.na;
+        task = req.body.ta
+        /*const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 1,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        
+        });*/
+        const completion = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'user',
+                    content: `${prompt}`
+                },
+            ]
+        })
+        res.status(200).send({
+            //bot: response.data.choices[0].text
+            bot: completion.data.choices[0].message
+        })
+        var client = new pg.Client(conString);
+        client.connect(function (err) {
+            if (err) {
+                return console.error('could not connect to postgres', err);
+            }
+        });
+        console.log("Verbunden");
+        //const data = await response.json();
+        const obj = completion.data.choices[0].message;
+        const parsedData   = obj.content.trim();
+        let sql = "INSERT INTO Prompt VALUES('" + req.body.prompt + "','" + parsedData + "','" + name + "','" + task + "');";
+        console.log(sql);
+        client.query(sql, function (err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            //console.log(result.rows[0].theTime);
+            // >> output: 2018-08-23T14:02:57.117Z
+            client.end();
+        });
+        /*var client = new pg.Client(conString)
+        client.connect(function (err) {
+            if (err) {
+                return console.error('could not connect to postgres', err);
+            }
+        });
+        console.log("Verbunden!")
+        let sql = "INSERT INTO Prompt VALUES('"+req.body.prompt+"','"+response.data.choices[0].text+"','"+name+"','" +task+"');";
+        client.query(sql, function(err, result) {
+            if(err) {
+              return console.error('error running query', err);
+            }
+            else{
+                console.log(result.rows);
+            }
+            client.end();
+          });*/
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ error });
+    }
+
+
+});
+
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'))

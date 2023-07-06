779bddcde13a509059fc3ae8d51ddae91eb55901
Sahil <sahillal25@outlook.de>
2023-07-06 12:44:30 +0200

    ..

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
     try {
-        const prompt = req.body.prompt;
+        const prompt = JSON.parse(req.body.prompt);
         name = req.body.na;
         task = req.body.ta
         /*const response = await openai.createCompletion({
@@ -49,12 +49,7 @@ app.post('/', async (req, res) => {
         });*/
         const completion = await openai.createChatCompletion({
             model: 'gpt-3.5-turbo',
-            messages: [
-                {
-                    role: 'user',
-                    content: `${prompt}`
-                },
-            ]
+            messages: prompt
         })
         res.status(200).send({
             //bot: response.data.choices[0].text
